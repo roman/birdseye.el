96db2619059aff256ee785ea3f329de54411e30a
Roman Gonzalez <roman@unbounce.com>
2014-08-26 17:23:13 -0700

    Modifying foldings for ruby

--- be-folding.el
+++ be-folding.el
@@ -3,7 +3,7 @@
 (be/util-eval-on-load ("hideshow-org" "ruby-mode")
   (add-to-list 'hs-special-modes-alist
                `(ruby-mode
-                 ,(rx (or "def" "if" (and (*? anything) "do") "{" "[")) ;; Block start
+                 ,(rx (or "def" "if" "do" "{" "[")) ;; Block start
                  ,(rx (or "}" "]" "end"))                  ;; Block end
                  ,(rx (or "#" "=begin"))                   ;; Comment start
                  ruby-forward-sexp nil)))
@@ -12,7 +12,7 @@
 (be/util-eval-on-load ("hideshow-org" "enh-ruby-mode")
   (add-to-list 'hs-special-modes-alist
                `(enh-ruby-mode
-                 ,(rx (or "def" "if" (and (*? anything) "do") "{" "[")) ;; Block start
+                 ,(rx (or "def" "if" "do" "{" "[")) ;; Block start
                  ,(rx (or "}" "]" "end"))           ;; Block end
                  ,(rx (or "#" "=begin"))            ;; Comment start
                  enh-ruby-forward-sexp nil)))
