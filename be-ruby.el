bcbb0bb3b9123ad2467f333979aa8426be268200
Roman Gonzalez <roman@unbounce.com>
2014-11-04 11:37:58 -0800

    Add evil symbol search on be-ruby

--- be-ruby.el
+++ be-ruby.el
@@ -4,21 +4,23 @@
   (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
     (rvm-activate-corresponding-ruby)))
 
-(be/util-eval-on-mode ruby-mode
+(defun be/ruby-mode-init-hook ()
   (setq ruby-insert-encoding-magic-comment nil)
   (linum-mode 1)
+  (be/util-eval-on-load ("evil")
+    ;; When using '*' or '#' search for symbols and
+    ;; not words
+    (setq evil-symbol-word-search t))
   (be/util-eval-on-load ("robe")
     (robe-mode 1))
   (be/util-eval-on-load ("hideshow-org")
     (hs-org/minor-mode 1)))
 
+(be/util-eval-on-mode ruby-mode
+  (be/ruby-mode-init-hook))
+
 (be/util-eval-on-mode enh-ruby-mode
-  (setq ruby-insert-encoding-magic-comment nil)
-  (linum-mode 1)
-  (be/util-eval-on-load ("robe")
-    (robe-mode 1))
-  (be/util-eval-on-load ("hideshow-org")
-    (hs-org/minor-mode 1)))
+  (be/ruby-mode-init-hook))
 
 
 (provide 'be-ruby)
